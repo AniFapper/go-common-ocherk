c997c3a9c26024b23a8062fd1503baba922e8aaf
AniFapper <viruns228555@gmail.com>
2026-04-16 22:05:24 +0300

    json marshallet

---
+++ contracts/marshaller/json.go
@@ -0,0 +1,15 @@
+package marshaller
+
+import (
+	"encoding/json"
+)
+
+type JSON struct{}
+
+func (j JSON) Marshal(v any) ([]byte, error) {
+	return json.Marshal(v)
+}
+
+func (j JSON) Unmarshal(data []byte, v any) error {
+	return json.Unmarshal(data, v)
+}
